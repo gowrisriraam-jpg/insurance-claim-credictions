7f08319349ca89b5daf8d94018bbd347a7d547c5
gowrisriraam-jpg <gowrisriraam@gmail.com>
2026-08-11 14:34:06 +0530

    Delete predict.py

--- predict.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.join(BASE_DIR, "insurance.csv")
-
-df = pd.read_csv(csv_path)                                                          
